8ca682da3af1d8dfdd4f2cecf2d6bdfd9ba512e6
unknown <nehemiahabelcjohan@gmail.com>
2025-12-03 17:31:44 +0000

    Finished Question 3

--- problem_inc_1.m
+++ problem_inc_1.m
@@ -2,10 +2,16 @@
 clear; close all;
 %% Mesh generation
 elementType='2dQ1'; %define element type
-domain=[0 0 4 1]; %[x0 y0 x1 y1]
+domain=[0 0 4 1]; %[x0 y0 x1 y1], 4-length and 1-height bar
 numElements=[15 10]; %[numElX numElY]
 [nodeCoords, IEN, boundaryElementIDs, boundaryNodeLocalID]=...
     meshRect2d(domain,elementType,numElements); %generate the mesh
+
+% nodeCoords result in a 2-column matrix like so:
+% [0 0;0.267 0;0.533 0;0.800 0; ...]
+% So column 1 is x-coord and column 2 is y-coord.
+% This means nodeCoords counts from bottom left,
+% and goes layer-by-layer upwards
 
 %% Parameters
 %elasticity tensor
@@ -23,16 +29,31 @@ Fb = zeros(numDoFs,1);
 % Boundary conditions
 leftNodes=find(nodeCoords(:,1)==domain(1)); 
 rightNodes=find(nodeCoords(:,1)==domain(3));
+% index of DoFs:
 leftXDoF=(leftNodes-1)*2+1;
 leftYDoF=(leftNodes-1)*2+2;
 rightXDoF=(rightNodes-1)*2+1;
 rightYDoF=(rightNodes-1)*2+2;
 
 prescribedDoF=[leftXDoF; leftYDoF]; %list of prescribed global DoFs
+
+% Also fix displacements at right boundary
+prescribedDoF=[prescribedDoF; rightXDoF; rightYDoF];
+% These allow us to define the Dirichlet boundary conditions to allow for
+% such a stretch (Question 2)
+
 freeDoF=setdiff(1:numDoFs,prescribedDoF); %list of free DoFs
-u_prescribed=zeros(numDoFs,1); 
+
+% Initialise a vector for displacement BCs
+u_prescribed=zeros(numDoFs,1);
+% Prescribed displacements
+u_prescribed(rightXDoF)=1e-7; %right edge moves 0.01 m to the right
+
 % Surface traction
 Fs = zeros(numDoFs,1); %define global force vector for surface traction
+% Traction free on top and bottom sides already implied since Fs is
+% initialised to zero anyway.
+
 
 %% Solution 
 F=Fb+Fs; %total load vector
